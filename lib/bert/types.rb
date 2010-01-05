523d05b90027abc616874bf0f3781f5f76e8834a
postmodern <postmodern.mod3@gmail.com>
2010-01-05 14:46:32 -0800

    Added the bert_data_type enum.

--- lib/bert/types.rb
+++ lib/bert/types.rb
@@ -9,6 +9,22 @@ module FFI
 
     typedef :uint, :time_t
 
+    enum :bert_data_type, [
+      :bert_data_none,
+      :bert_data_boolean,
+      :bert_data_int,
+      :bert_data_float,
+      :bert_data_atom,
+      :bert_data_string,
+      :bert_data_tuple,
+      :bert_data_list,
+      :bert_data_dict,
+      :bert_data_bin,
+      :bert_data_time,
+      :bert_data_regex,
+      :bert_data_nil
+    ]
+
     callback :bert_read_func, [:pointer, :size_t, :pointer], :ssize_t
     callback :bert_write_func, [:pointer, :size_t, :pointer], :ssize_t
   end
