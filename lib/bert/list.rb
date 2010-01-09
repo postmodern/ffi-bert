b1b673b44190a258c6c196578dcaa9fcd95e4d23
postmodern <postmodern.mod3@gmail.com>
2010-01-08 23:54:10 -0800

    Added List#<<.

--- lib/bert/list.rb
+++ lib/bert/list.rb
@@ -1,4 +1,6 @@
+require 'bert/errno'
 require 'bert/list_node'
+require 'bert/ffi'
 
 require 'ffi'
 
@@ -6,8 +8,18 @@ module FFI
   module BERT
     class List < FFI::Struct
 
+      include Errno
+
       layout :head, :pointer,
              :tail, :pointer
+
+      def <<(data)
+        if BERT.bert_list_append(self,data) == Errno::MALLOC
+          raise_error(Errno::MALLOC)
+        end
+
+        return self
+      end
 
     end
   end
