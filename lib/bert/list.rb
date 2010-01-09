39c48bfa8dc86d95010ddda80ac7659117dc0368
postmodern <postmodern.mod3@gmail.com>
2010-01-09 00:04:16 -0800

    Added List#each.

--- lib/bert/list.rb
+++ lib/bert/list.rb
@@ -8,10 +8,25 @@ module FFI
   module BERT
     class List < FFI::Struct
 
+      include Enumerable
       include Errno
 
       layout :head, :pointer,
              :tail, :pointer
+
+      def each(&block)
+        head_ptr = self[:head]
+        return self if head_ptr.null?
+
+        next_node = ListNode.new(head_ptr)
+
+        while next_node
+          block.call(next_node.data) if block
+          next_node = next_node.next
+        end
+
+        return self
+      end
 
       def <<(data)
         if BERT.bert_list_append(self,data) == Errno::MALLOC
