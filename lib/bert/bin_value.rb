06021afd88913706515d0a648a77a3be66cd2b55
postmodern <postmodern.mod3@gmail.com>
2010-01-08 23:29:22 -0800

    Added BinValue#data.

--- lib/bert/bin_value.rb
+++ lib/bert/bin_value.rb
@@ -9,6 +9,12 @@ module FFI
       layout :length, :bert_bin_size_t,
              :data, :pointer
 
+      def data
+        self[:data].get_bytes(self[:length])
+      end
+
+      alias to_s data
+
     end
   end
 end
