3c2e199adbbeaf16a3275d96cc437977a68c5181
postmodern <postmodern.mod3@gmail.com>
2010-01-09 01:55:04 -0800

    Added Dict#[]=.

--- lib/bert/dict.rb
+++ lib/bert/dict.rb
@@ -1,4 +1,5 @@
 require 'bert/dict_node'
+require 'bert/errno'
 
 require 'ffi'
 
@@ -6,8 +7,18 @@ module FFI
   module BERT
     class Dict < FFI::Struct
 
+      include Errno
+
       layout :head, :pointer,
              :tail, :pointer
+
+      def []=(key,value)
+        if BERT.bert_dict_append(self,key,value) == Errno::MALLOC
+          raise_error(Errno::MALLOC)
+        end
+
+        return value
+      end
 
     end
   end
